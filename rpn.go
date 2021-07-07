2a8ba2ac24f6c72f61681603bc68e116613c92fa
Shintaro Kita <kitasin101021@gmail.com>
2021-07-07 01:48:46 +0000

    Adding add function

--- rpn.go
+++ rpn.go
@@ -5,14 +5,25 @@ import (
 	//"bytes"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
+
+func add(l []string) int {
+	var x [2]int
+	x[0], _ = strconv.Atoi(l[0])
+	x[1], _ = strconv.Atoi(l[1])
+
+	result := x[0] + x[1]
+	return result
+}
 
 func main() {
 	s := bufio.NewScanner(os.Stdin)
 	s.Scan()
 	input := s.Text()
 	l := strings.Split(input, " ")
-	fmt.Println(l[1])
+
+	fmt.Println(add(l))
 
 }
--- rpn_test.go
+++ rpn_test.go
@@ -1,3 +1,12 @@
 package main
 
 import "testing"
+
+func TestEx01(t *testing.T) {
+	expect := 3
+	slice := []string{"1", "2", "+"}
+	result := add(slice)
+	if result != expect {
+		t.Error("Test01 is failed")
+	}
+}
