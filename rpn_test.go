d9b9c85104e8bb5036d729f327a156f0b8274655
Shintaro Kita <kitasin101021@gmail.com>
2021-07-07 02:38:07 +0000

    add v3

--- rpn_test.go
+++ rpn_test.go
@@ -2,6 +2,7 @@ package main
 
 import "testing"
 
+/*
 func TestEx01(t *testing.T) {
 	expect := 3
 	slice := []string{"1", "2", "+"}
@@ -9,6 +10,7 @@ func TestEx01(t *testing.T) {
 	if result != expect {
 		t.Error("Test01 is failed")
 	}
+
 }
 
 func TestEx02(t *testing.T) {
@@ -26,5 +28,14 @@ func TestEx03(t *testing.T) {
 	result := calculater(slice)
 	if result != expect {
 		t.Error("Test03 is failed")
+	}
+}
+*/
+func TestEx04(t *testing.T) {
+	expect := 2
+	slice := []string{"3", "2", "*"}
+	result := search(slice)
+	if result != expect {
+		t.Error("Test04 is failed")
 	}
 }
